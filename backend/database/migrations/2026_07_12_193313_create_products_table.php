617554129238ee577f1c9762ab1582208b2be940
Mouloud78 <mouloudouldali78@gmail.com>
2026-07-12 15:53:44 -0400

    all fields added and migration added

--- backend/database/migrations/2026_07_12_193313_create_products_table.php
+++ backend/database/migrations/2026_07_12_193313_create_products_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('qty');
+            $table->integer('price');
+            $table->longText('desc');
+            $table->string('thumbnail');
+            $table->string('first_image')->nullable();
+            $table->string('second_image')->nullable();
+            $table->string('third_image')->nullable();
+            $table->boolean('status')->default(1);
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
